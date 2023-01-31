4f5fe6122667ab0267b070447073cf43720406a1
x2ecom <jeff@x2ecom.com>
2023-01-31 17:01:22 -0500

    Fix for jwt

--- lib/datasources/http/http.dart
+++ lib/datasources/http/http.dart
@@ -196,7 +196,7 @@ class Http
       _headers[HttpHeaders.ageHeader] = '0';
       _headers[HttpHeaders.contentEncodingHeader] = 'utf8';
       _headers[HttpHeaders.contentTypeHeader] = "application/xml";
-      if (System.app?.jwt != null) _headers[HttpHeaders.authorizationHeader] = "Bearer ${System.app!.jwt}";
+      if (System.app?.jwt?.token != null) _headers[HttpHeaders.authorizationHeader] = "Bearer ${System.app!.jwt!.token}";
     }
     else headers.forEach((key, value) => _headers[key] = value);
     return _headers;

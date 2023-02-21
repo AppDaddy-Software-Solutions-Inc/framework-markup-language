79a942d68d70f45ec1d1a356b25c585c59341890
BrayFlex <flexmans@hotmail.com>
2023-02-21 15:45:50 -0500

    add code for avail cameras exception

--- lib/widgets/camera/camera_view.dart
+++ lib/widgets/camera/camera_view.dart
@@ -157,6 +157,7 @@ class CameraViewState extends State<CameraView>
           default:
           // Handle other errors here.
             widget.model.onFail(Data(), message: "Unable to get any available Cameras");
+            Log().exception('Unable to get any available Cameras - ${e.code}: ${e.toString()}');
             break;
         }
       }

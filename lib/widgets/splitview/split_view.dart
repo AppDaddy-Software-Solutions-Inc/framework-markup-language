9fc4b4425af146952684560e780a37e9c60fb782
x2ecom <jeff@x2ecom.com>
2023-02-27 12:49:41 -0500

    Added <SPLITVIEW direction="vertical | horizontal" (default horizontal) as well as the ability to set width (left panel size on horizontal) or height (top panel size on vertical)

--- lib/widgets/splitview/split_view.dart
+++ lib/widgets/splitview/split_view.dart
@@ -21,21 +21,6 @@ class SplitView extends StatefulWidget
 
 class _SplitViewState extends State<SplitView> implements IModelListener
 {
-  double _dividerWidth = 6.0;
-  Color? _dividerColor;
-
-  double _ratio = 0.5;
-  set ratio (double v)
-  {
-    if (v < 0) v = 1;
-    if (v > 1) v = 1;
-    _ratio = v;
-  }
-
-  double? _maxWidth;
-  get _width1 => _ratio * _maxWidth!;
-  get _width2 => (1 - _ratio) * _maxWidth!;
-
   @override
   void initState()
   {
@@ -44,7 +29,6 @@ class _SplitViewState extends State<SplitView> implements IModelListener
 
     // If the model contains any databrokers we fire them before building so we can bind to the data
     widget.model.initialize();
-    ratio = widget.model.width != null ? widget.model.width! / (widget.model.maxWidth! - _dividerWidth) : widget.model.ratio;
   }
 
   @override
@@ -70,8 +54,8 @@ class _SplitViewState extends State<SplitView> implements IModelListener
   }
 
   /// Callback to fire the [_BoxViewState.build] when the [BoxModel] changes
-  onModelChange(WidgetModel model, {String? property, dynamic value}) {
-    ratio = widget.model.width != null ? widget.model.width! / (widget.model.maxWidth! - _dividerWidth) : widget.model.ratio;
+  onModelChange(WidgetModel model, {String? property, dynamic value})
+  {
     if (this.mounted) setState(() {});
   }
 
@@ -102,13 +86,11 @@ class _SplitViewState extends State<SplitView> implements IModelListener
     widget.model.minHeight = constraints.minHeight;
     widget.model.maxHeight = constraints.maxHeight;
 
-    _dividerWidth = widget.model.dividerWidth ?? (System().useragent == 'desktop' || S.isNullOrEmpty(System().useragent) ? 6.0 : 12.0);
-    _dividerColor = widget.model.dividerColor ?? Theme.of(context).colorScheme.onInverseSurface;
+    var _dividerWidth = widget.model.dividerWidth ?? (System().useragent == 'desktop' || S.isNullOrEmpty(System().useragent) ? 6.0 : 12.0);
+    if (_dividerWidth % 2 != 0) _dividerWidth = _dividerWidth + 1;
 
-    if (_maxWidth == null)                  _maxWidth = widget.model.maxWidth! - _dividerWidth;
-    if (_maxWidth != widget.model.maxWidth) _maxWidth = widget.model.maxWidth! - _dividerWidth;
-    if (_maxWidth != null && _maxWidth! < 0) _maxWidth = 0;
-
+    var _dividerColor = widget.model.dividerColor ?? Theme.of(context).colorScheme.onInverseSurface;
+    
     // views
     if (widget.views.isEmpty)
     widget.model.children?.forEach((child)
@@ -116,23 +98,59 @@ class _SplitViewState extends State<SplitView> implements IModelListener
       if (child is ViewModel) widget.views.add(child.getView());
     });
 
-    // left pane
-    var left = SizedBox(width: _width1, child: Container(color: Theme.of(context).colorScheme.surface, child: (widget.views.isNotEmpty ? widget.views[0] : Text ('Missing <View />'))));
+
+    // calculate sizes
+    var size1 = (widget.model.vertical ? widget.model.height ?? 0 : widget.model.width  ?? 0);
+    var size2 = (widget.model.vertical ? widget.model.maxHeight! : widget.model.maxWidth!) - size1;
+    size1 = size1 - (_dividerWidth/2);
+    size2 = size2 - (_dividerWidth/2);
+    if (size1 < 0)
+    {
+      size2 = size2 + size1;
+      size1 = 0;
+    }
+    if (size2 < 0)
+    {
+      size1 = size1 + size2;
+      size2 = 0;
+    }
+
+    // left/top pane
+    var pane1 = widget.model.vertical ?
+    SizedBox(height: size1, child: Container(color: Theme.of(context).colorScheme.surface, child: (widget.views.isNotEmpty ? widget.views[0] : Text ('Missing <View />')))) :
+    SizedBox(width:  size1, child: Container(color: Theme.of(context).colorScheme.surface, child: (widget.views.isNotEmpty ? widget.views[0] : Text ('Missing <View />'))));
 
     // handle
-    var handle = GestureDetector(behavior: HitTestBehavior.opaque, onHorizontalDragUpdate: _onDrag,
-        child: Container(color: _dividerColor, child: SizedBox(width: _dividerWidth, height: constraints.maxHeight, child: MouseRegion(cursor: SystemMouseCursors.resizeLeftRight, child: RotationTransition(child: Icon(Icons.drag_handle, color: widget.model.dividerHandleColor), turns: AlwaysStoppedAnimation(.25))))));
+    Widget handle = widget.model.vertical ?
+    GestureDetector(behavior: HitTestBehavior.opaque, onVerticalDragUpdate:   _onDrag, child: Container(color: _dividerColor, child: SizedBox(width: constraints.maxWidth, height: _dividerWidth, child: MouseRegion(cursor: SystemMouseCursors.resizeUpDown,     child: Stack(children: [Positioned(top: -10, child: Icon(Icons.drag_handle, color: widget.model.dividerHandleColor))]))))) :
+    GestureDetector(behavior: HitTestBehavior.opaque, onHorizontalDragUpdate: _onDrag, child: Container(color: _dividerColor, child: SizedBox(width: _dividerWidth, height: constraints.maxHeight, child: MouseRegion(cursor: SystemMouseCursors.resizeLeftRight, child: RotationTransition(child: Icon(Icons.drag_handle, color: widget.model.dividerHandleColor), turns: AlwaysStoppedAnimation(.25))))));
 
-    // right pane
-    var right = SizedBox(width: _width2, child: Container(color: Theme.of(context).colorScheme.surface, child: (widget.views.length > 1 ? widget.views[1] : Text ('Missing <View />'))));
+    // right/bottom pane
+    var pane2 = widget.model.vertical ?
+    SizedBox(height: size2, child: Container(color: Theme.of(context).colorScheme.surface, child: (widget.views.length > 1 ? widget.views[1] : Text ('Missing <View />')))) :
+    SizedBox(width: size2, child: Container(color: Theme.of(context).colorScheme.surface, child: (widget.views.length > 1 ? widget.views[1] : Text ('Missing <View />'))));
 
     // view
-    return SizedBox(width: constraints.maxWidth, child: Row(children: <Widget>[left, handle, right]));
+    return widget.model.vertical ?
+    SizedBox(height: constraints.maxHeight, child: Column(children: <Widget>[pane1, handle, pane2])) :
+    SizedBox(width: constraints.maxWidth,   child: Row(children: <Widget>[pane1, handle, pane2]));
   }
 
   void _onDrag(DragUpdateDetails details)
   {
-    /// print('hor drag' + details.delta.dx.toString() + ' | ' + _ratio.toString() + ' w1: ' + _width1.toString() + ' w2: ' + _width2.toString());
-    setState(() { _ratio += details.delta.dx / _maxWidth!; if (_ratio > 1) _ratio = 1; else if (_ratio < 0.0) _ratio = 0.0; });
+    if (widget.model.vertical)
+    {
+      var height = (widget.model.height ?? 0) + details.delta.dy;
+      if (height < 0) height = 0;
+      if (height > widget.model.maxHeight!) height = widget.model.maxHeight!;
+      widget.model.height = height;
+    }
+    else
+    {
+      var width = (widget.model.width ?? 0) + details.delta.dx;
+      if (width < 0) width = 0;
+      if (width > widget.model.maxWidth!) width = widget.model.maxWidth!;
+      widget.model.width = width;
+    }
   }
 }

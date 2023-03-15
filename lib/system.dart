d01cbb354dd494d5f936982b34a3cb1572879a94
Isaac Olajos <isaacolajos@hotmail.com>
2023-03-15 09:58:19 -0400

    s

--- lib/system.dart
+++ lib/system.dart
@@ -37,7 +37,7 @@ final String applicationTitle = "Flutter Markup Language " + version;
 // Used in SingleApp only and on Web when developing on localhost
 // Set this to file://applications/<app> to use the asset applications
 
-String get defaultDomain => 'http://test.appdaddy.co';
+String get defaultDomain => 'https://test.appdaddy.co';
 
 // SingleApp - App initializes from a single domain endpoint (defined in defaultDomain)
 // MultiApp  - (Desktop & Mobile Only) Launches the Store at startup

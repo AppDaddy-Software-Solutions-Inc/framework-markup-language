12416dd51612f3e64f0ae6b8e958db0a18515771
BrayFlex <flexmans@hotmail.com>
2023-03-24 09:29:59 -0400

    commit for jeff

--- lib/datasources/transforms/filter.dart
+++ lib/datasources/transforms/filter.dart
@@ -3,6 +3,7 @@ import 'package:fml/data/data.dart';
 import 'package:fml/datasources/transforms/iTransform.dart';
 import 'package:fml/datasources/transforms/transform_model.dart';
 import 'package:fml/eval/eval.dart';
+import 'package:fml/system.dart';
 import 'package:xml/xml.dart';
 import 'package:fml/widgets/widget/widget_model.dart'  ;
 import 'package:fml/observable/observable_barrel.dart';
@@ -12,13 +13,17 @@ class Filter extends TransformModel implements ITransform
 {
   // enabled
   BooleanObservable? _enabled;
+
+  Scope? externalScope;
+
   set enabled(dynamic v) {
     if (_enabled != null) {
       _enabled!.set(v);
     }
     else if (v != null)
     {
-      _enabled = BooleanObservable(Binding.toKey(id, 'enabled'), v, scope: scope, listener: onPropertyChange);
+      _enabled = BooleanObservable(Binding.toKey(id, 'enabled'), v, scope: externalScope, listener: onPropertyChange);
+      _enabled!.registerListener(onPropertyChange);
     }
   }
   bool get enabled => _enabled?.get() ?? true;
@@ -30,40 +35,39 @@ class Filter extends TransformModel implements ITransform
     ////////////
     /* Filter */
     ////////////
-    if (v is String) v = encode(v);
     if (_filter != null)
     {
       _filter!.set(v);
     }
     else if (v != null)
     {
-      _filter = StringObservable(Binding.toKey(id, 'filter'), v, scope: scope, listener: onPropertyChange);
+      _filter = StringObservable(Binding.toKey(id, 'filter'), v, scope: externalScope, listener: onFilterChange);
+      _filter!.registerListener(onFilterChange);
     }
   }
   String? get filter
   {
     String? v =_filter?.get();
-    if (v != null) v = decode(v);
     return v;
   }
 
-  Filter(WidgetModel? parent, {String? id, dynamic enabled, dynamic filter}) : super(parent, id)
+  Filter(WidgetModel? parent, Scope? externalScope, {String? id, dynamic enabled, dynamic filter}) : super(parent, id)
   {
+    this.externalScope = externalScope;
     this.enabled  = enabled;
     this.filter   = filter;
   }
 
-  static Filter? fromXml(WidgetModel? parent, XmlElement xml)
+  static Filter? fromXml(WidgetModel? parent, XmlElement xml, Scope? externalScope)
   {
     String? id = Xml.get(node: xml, tag: 'id');
     if (S.isNullOrEmpty(id)) id = S.newId();
-
-    Filter model = Filter
-      (
+    Filter model = Filter(
         parent,
-        id      : id,
-        enabled : Xml.get(node: xml, tag: 'enabled'),
-        filter  : Xml.get(node: xml, tag: 'filter')
+        externalScope,
+        id: id,
+        enabled: Xml.get(node: xml, tag: 'enabled'),
+        filter : Xml.get(node: xml, tag: 'filter')
     );
     model.deserialize(xml);
     return model;
@@ -72,45 +76,50 @@ class Filter extends TransformModel implements ITransform
   @override
   void deserialize(XmlElement xml)
   {
-
+    _enabled?.removeListener(onPropertyChange);
+    _filter?.removeListener(onFilterChange);
     // Deserialize
     super.deserialize(xml);
   }
 
   _fromList(Data? data)
   {
-    if (data == null) return null;
+    if (data == null || filter == null) return null;
 
     var bindings = Binding.getBindings(filter);
+
+    // Out of Scope Variables, not local to the data row
+    var oosVariables = Map<String?, dynamic>();
+    bindings?.forEach((b) {
+      if (b.source != 'data')
+        oosVariables[b.signature] = externalScope?.getObservable(b, requestor: _filter)?.value;
+    });
 
     List remove = [];
     data.forEach((row)
     {
-      var variables = Data.readValues(bindings, row);
-      var result = Eval.evaluate(filter, variables: variables);
+      // Data Row Scoped Variables, local to the data row
+      var rowVariables = Data.readValues(bindings, row);
 
+      // Combine the local and out of scope variables so the row can utilize both within an eval
+      rowVariables.addAll(oosVariables);
+
+      // Evaluate the filter
+      var result = Eval.evaluate(filter, variables: rowVariables);
+
+      // Based on the eval create a remove list
       bool? ok = S.toBool(result);
       if (ok != true) remove.add(row);
     });
+
+    // Filter the results out
     data.removeWhere((row) => remove.contains(row));
   }
 
-  String encode(String v)
+  void onFilterChange(Observable observable)
   {
-    List<String?>? bindings = Binding.getBindingStrings(v);
-    if (bindings != null)
-      bindings.forEach((binding)
-      {
-        if (!binding!.contains(".")) v = v.replaceAll(binding, binding.replaceAll("{", "[[[[").replaceAll("}", "]]]]"));
-      });
-    return v;
-  }
-
-  String decode(String v)
-  {
-    v = v.replaceAll("[[[[", "{");
-    v = v.replaceAll("]]]]", "}");
-    return v;
+    print('################################################################');
+    apply(data);
   }
 
   apply(Data? data) async
@@ -118,4 +127,5 @@ class Filter extends TransformModel implements ITransform
     if (enabled == false) return;
     _fromList(data);
   }
+
 }
--- lib/system.dart
+++ lib/system.dart
@@ -37,11 +37,11 @@ final String applicationTitle = "Flutter Markup Language " + version;
 // Used in SingleApp only and on Web when developing on localhost
 // Set this to file://applications/<app> to use the asset applications
 
-String get defaultDomain => 'https://pad.fml.dev';
+String get defaultDomain => 'https://test.appdaddy.co/#/resources/debug.xml';
 
 // SingleApp - App initializes from a single domain endpoint (defined in defaultDomain)
 // MultiApp  - (Desktop & Mobile Only) Launches the Store at startup
-final ApplicationTypes appType = ApplicationTypes.MultiApp;
+final ApplicationTypes appType = ApplicationTypes.SingleApp;
 
 enum ApplicationTypes{ SingleApp, MultiApp }
 

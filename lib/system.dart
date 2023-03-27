a66ff76e148007ae116a2f6ccf989bcb7db92d3a
BrayFlex <flexmans@hotmail.com>
2023-03-27 09:12:18 -0400

    sys

--- lib/system.dart
+++ lib/system.dart
@@ -37,11 +37,11 @@ final String applicationTitle = "Flutter Markup Language " + version;
 // Used in SingleApp only and on Web when developing on localhost
 // Set this to file://applications/<app> to use the asset applications
 
-String get defaultDomain => 'https://test.appdaddy.co/#/resources/debug.xml';
+String get defaultDomain => 'https://fml.dev';
 
 // SingleApp - App initializes from a single domain endpoint (defined in defaultDomain)
 // MultiApp  - (Desktop & Mobile Only) Launches the Store at startup
-final ApplicationTypes appType = ApplicationTypes.SingleApp;
+final ApplicationTypes appType = ApplicationTypes.MultiApp;
 
 enum ApplicationTypes{ SingleApp, MultiApp }
 

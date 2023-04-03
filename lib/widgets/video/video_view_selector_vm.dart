bcb27dc806f13d02101da80cac98579598a96881
BrayFlex <flexmans@hotmail.com>
2023-04-03 13:45:37 -0400

    uncomment

--- lib/widgets/video/video_view_selector_vm.dart
+++ lib/widgets/video/video_view_selector_vm.dart
@@ -1,15 +1,15 @@
-// import 'package:flutter/material.dart';
-// import 'package:fml/system.dart';
-// import 'package:fml/widgets/video/video_model.dart';
-// import 'package:fml/widgets/video/video_view.dart';
-// import 'package:fml/widgets/video/video_view_vlc.dart';
-//
-// class VideoPlayerSelector
-// {
-//   static Widget getView(VideoModel model)
-//   {
-//     if (System().useragent == "windows" || System().useragent == "linux")
-//          return VideoViewVlc(model);
-//     else return VideoView(model);
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:fml/system.dart';
+import 'package:fml/widgets/video/video_model.dart';
+import 'package:fml/widgets/video/video_view.dart';
+import 'package:fml/widgets/video/video_view_vlc.dart';
+
+class VideoPlayerSelector
+{
+  static Widget getView(VideoModel model)
+  {
+    if (System().useragent == "windows" || System().useragent == "linux")
+         return VideoViewVlc(model);
+    else return VideoView(model);
+  }
+}

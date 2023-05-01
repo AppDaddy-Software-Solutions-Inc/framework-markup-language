04a159b77b7eb650c65413877b471f64fb4c3955
BrayFlex <flexmans@hotmail.com>
2023-05-01 15:38:11 -0400

    fix accentcolor binding

--- lib/observable/observables/color.dart
+++ lib/observable/observables/color.dart
@@ -396,6 +396,7 @@ class ColorObservable extends Observable
         else if (color.length == 10 && color.startsWith('0x')) c = Color(int.parse(color.substring(2, 10), radix: 16) + 0x00000000);
         else if (color.length == 17 && color.startsWith('Color(0x')) c = Color(int.parse(color.substring(8, 16), radix: 16) + 0x00000000);
         else if (color.length == 47 && color.startsWith('MaterialColor(primary value: Color(0x')) c = Color(int.parse(color.substring(37, 45), radix: 16) + 0x00000000);
+        else if (color.length == 53 && color.startsWith('MaterialAccentColor(primary value: Color(0x')) c = Color(int.parse(color.substring(43, 51), radix: 16) + 0x00000000);
       }
     }
     catch(e)

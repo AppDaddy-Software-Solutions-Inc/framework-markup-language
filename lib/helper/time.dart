1e21fb8f59ae18dfa8c904765cb701da5d672fc1
BrayFlex <flexmans@hotmail.com>
2023-05-02 13:38:24 -0400

    docomentation

--- lib/helper/time.dart
+++ lib/helper/time.dart
@@ -4,6 +4,7 @@ enum TimeUnit {
   millisecond, second, minute, hour, day, week, month, year;
   const TimeUnit();
 
+  /// Returns [TimeUnit]s as milliseconds (except month/year)
   double asMs() {
     switch(this) {
       case TimeUnit.millisecond:
@@ -25,36 +26,50 @@ enum TimeUnit {
 
 }
 
+/// Time Helper Class
 class T {
 
+  /// Given a string such as '100ms' or '10 years' this function returns a
+  /// [TimeUnitDuration] object
   static TimeUnitDuration getTUDurationFromString(String str) {
     return TimeUnitDuration.fromString(str);
   }
 
+  /// Formats a date/time string
   static String formatFromString(String datetimeStr, String inputFormat, String outputFormat) =>
       Jiffy(datetimeStr, inputFormat).format(outputFormat);
 
+  /// Formats a [DateTime] into a String
   static String formatFromDateTime(DateTime datetime, String outputFormat) =>
       Jiffy(datetime).format(outputFormat);
 
-  static String timeBetweenPrettyPrint(DateTime a, DateTime b) =>
+  /// Returns a human readable string of the time between 2 [DateTime]s
+  /// ex: '1 minute ago' or 'in 2 days'
+  static String timeBetween(DateTime a, DateTime b) =>
       Jiffy(a).from(Jiffy(b));
 
+  /// Compare if DateTime a is before DateTime b
   static bool isBefore(DateTime a, DateTime b) =>
       Jiffy(a).isBefore(b);
 
+  /// Compare if DateTime a is after DateTime b
   static bool isAfter(DateTime a, DateTime b) =>
       Jiffy(a).isAfter(b);
 
+
+  /// Compare if DateTime a is before DateTime b or the same
   static bool isSameOrBefore(DateTime a, DateTime b) =>
       Jiffy(a).isSameOrBefore(b);
 
+  /// Compare if DateTime a is after DateTime b or the same
   static bool isSameOrAfter(DateTime a, DateTime b) =>
       Jiffy(a).isSameOrAfter(b);
 
+  /// Compare if DateTime a the same as DateTime b
   static bool isSame(DateTime a, DateTime b) =>
       Jiffy(a).isSame(b);
 
+  /// Adds a [TimeUnitDuration] to a [DateTime]
   static DateTime add(DateTime dateTime, TimeUnitDuration tud) =>
       Jiffy(dateTime).add(
           milliseconds: tud.timeUnit == TimeUnit.millisecond ? tud.amount : 0,
@@ -66,6 +81,7 @@ class T {
           years: tud.timeUnit == TimeUnit.year ? tud.amount : 0
       ).dateTime;
 
+  /// Subtracts a [TimeUnitDuration] from a [DateTime]
   static DateTime subtract(DateTime dateTime, TimeUnitDuration tud) =>
       Jiffy(dateTime).subtract(
           milliseconds: tud.timeUnit == TimeUnit.millisecond ? tud.amount : 0,
@@ -79,6 +95,9 @@ class T {
 
 }
 
+/// Specifies a custom time unit using familiar [TimeUnit]s and an amount to
+/// establish the duration period.
+/// Returns 0 [TimeUnit.millisecond]s if the parsing fails
 class TimeUnitDuration {
   late int amount;
   late TimeUnit timeUnit;
@@ -86,10 +105,10 @@ class TimeUnitDuration {
   TimeUnitDuration(this.amount, this.timeUnit);
 
   TimeUnitDuration.fromString(String str) {
-    String interval = str.toString().trim();
+    String tudString = str.toString().trim();
     List<dynamic> matches = [
       ...RegExp(r'\d+|[A-Za-z]+')
-          .allMatches(interval).map((match) => match[0]!)
+          .allMatches(tudString).map((match) => match[0]!)
           .map((string) => int.tryParse(string) ?? string)
     ];
     int? amt;
@@ -164,10 +183,13 @@ class TimeUnitDuration {
     }
   }
 
+  /// Returns the [TimeUnitDuration] in milliseconds
   double asMs() {
     return amount * timeUnit.asMs();
   }
 
+  /// Returns the [TimeUnitDuration] as a string
+  @override
   String toString() {
     return '${amount.toString()} ${timeUnit.name}${amount > 0 ? 's' : ''}';
   }

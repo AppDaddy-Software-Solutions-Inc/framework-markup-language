de2144110782372b5b78d3694eb72aafb0828b56
BrayFlex <flexmans@hotmail.com>
2023-05-02 13:23:50 -0400

    Added Time helper Class

---
+++ lib/helper/time.dart
@@ -0,0 +1,175 @@
+import 'package:jiffy/jiffy.dart';
+
+enum TimeUnit {
+  millisecond, second, minute, hour, day, week, month, year;
+  const TimeUnit();
+
+  double asMs() {
+    switch(this) {
+      case TimeUnit.millisecond:
+        return 1;
+      case TimeUnit.second:
+        return 1000;
+      case TimeUnit.minute:
+        return 60000;
+      case TimeUnit.hour:
+        return 3600000;
+      case TimeUnit.day:
+        return 86400000;
+      case TimeUnit.week:
+        return 604800000;
+      default: // Unable to give precise values for month/year
+        return 0;
+    }
+  }
+
+}
+
+class T {
+
+  static TimeUnitDuration getTUDurationFromString(String str) {
+    return TimeUnitDuration.fromString(str);
+  }
+
+  static String formatFromString(String datetimeStr, String inputFormat, String outputFormat) =>
+      Jiffy(datetimeStr, inputFormat).format(outputFormat);
+
+  static String formatFromDateTime(DateTime datetime, String outputFormat) =>
+      Jiffy(datetime).format(outputFormat);
+
+  static String timeBetweenPrettyPrint(DateTime a, DateTime b) =>
+      Jiffy(a).from(Jiffy(b));
+
+  static bool isBefore(DateTime a, DateTime b) =>
+      Jiffy(a).isBefore(b);
+
+  static bool isAfter(DateTime a, DateTime b) =>
+      Jiffy(a).isAfter(b);
+
+  static bool isSameOrBefore(DateTime a, DateTime b) =>
+      Jiffy(a).isSameOrBefore(b);
+
+  static bool isSameOrAfter(DateTime a, DateTime b) =>
+      Jiffy(a).isSameOrAfter(b);
+
+  static bool isSame(DateTime a, DateTime b) =>
+      Jiffy(a).isSame(b);
+
+  static DateTime add(DateTime dateTime, TimeUnitDuration tud) =>
+      Jiffy(dateTime).add(
+          milliseconds: tud.timeUnit == TimeUnit.millisecond ? tud.amount : 0,
+          seconds: tud.timeUnit == TimeUnit.second ? tud.amount : 0,
+          minutes: tud.timeUnit == TimeUnit.minute ? tud.amount : 0,
+          hours: tud.timeUnit == TimeUnit.hour ? tud.amount : 0,
+          days: tud.timeUnit == TimeUnit.day ? tud.amount : 0,
+          months: tud.timeUnit == TimeUnit.month ? tud.amount : 0,
+          years: tud.timeUnit == TimeUnit.year ? tud.amount : 0
+      ).dateTime;
+
+  static DateTime subtract(DateTime dateTime, TimeUnitDuration tud) =>
+      Jiffy(dateTime).subtract(
+          milliseconds: tud.timeUnit == TimeUnit.millisecond ? tud.amount : 0,
+          seconds: tud.timeUnit == TimeUnit.second ? tud.amount : 0,
+          minutes: tud.timeUnit == TimeUnit.minute ? tud.amount : 0,
+          hours: tud.timeUnit == TimeUnit.hour ? tud.amount : 0,
+          days: tud.timeUnit == TimeUnit.day ? tud.amount : 0,
+          months: tud.timeUnit == TimeUnit.month ? tud.amount : 0,
+          years: tud.timeUnit == TimeUnit.year ? tud.amount : 0
+      ).dateTime;
+
+}
+
+class TimeUnitDuration {
+  late int amount;
+  late TimeUnit timeUnit;
+
+  TimeUnitDuration(this.amount, this.timeUnit);
+
+  TimeUnitDuration.fromString(String str) {
+    String interval = str.toString().trim();
+    List<dynamic> matches = [
+      ...RegExp(r'\d+|[A-Za-z]+')
+          .allMatches(interval).map((match) => match[0]!)
+          .map((string) => int.tryParse(string) ?? string)
+    ];
+    int? amt;
+    String? time;
+    if (matches.length >= 2 && matches[0].runtimeType == int && matches[1].runtimeType == String) {
+      amt = matches[0]!;
+      time = matches[1]!;
+    }
+    if (amt == null || time == null) {
+      this.amount = 0;
+      this.timeUnit = TimeUnit.millisecond;
+    }
+    else {
+      switch (time.trim().toLowerCase()) {
+        case 'ms':
+        case 'millisecond':
+        case 'milliseconds':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.millisecond;
+          break;
+        case 's':
+        case 'sec':
+        case 'second':
+        case 'seconds':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.second;
+          break;
+        case 'm':
+        case 'min':
+        case 'minute':
+        case 'minutes':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.minute;
+          break;
+        case 'h':
+        case 'hr':
+        case 'hour':
+        case 'hours':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.hour;
+          break;
+        case 'd':
+        case 'day':
+        case 'days':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.day;
+          break;
+        case 'w':
+        case 'week':
+        case 'weeks':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.week;
+          break;
+        case 'mo':
+        case 'month':
+        case 'months':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.month;
+          break;
+        case 'y':
+        case 'yr':
+        case 'year':
+        case 'years':
+        this.amount = amt;
+        this.timeUnit = TimeUnit.year;
+          break;
+        default:
+          this.amount = 0;
+          this.timeUnit = TimeUnit.millisecond;
+          break;
+      }
+    }
+  }
+
+  double asMs() {
+    return amount * timeUnit.asMs();
+  }
+
+  String toString() {
+    return '${amount.toString()} ${timeUnit.name}${amount > 0 ? 's' : ''}';
+  }
+
+}

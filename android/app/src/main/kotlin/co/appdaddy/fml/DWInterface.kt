8b76d02f4fafb2f318d65d42619c93433dae606c
x2ecom <jeff@x2ecom.com>
2023-10-16 14:39:33 -0400

    Updates to data wedge

--- android/app/src/main/kotlin/co/appdaddy/fml/DWInterface.kt
+++ android/app/src/main/kotlin/co/appdaddy/fml/DWInterface.kt
@@ -11,6 +11,7 @@ class DWInterface() {
         const val DATAWEDGE_RETURN_CATEGORY = "android.intent.category.DEFAULT"
         const val DATAWEDGE_EXTRA_SEND_RESULT = "SEND_RESULT"
         const val DATAWEDGE_SCAN_EXTRA_DATA_STRING = "com.symbol.datawedge.data_string"
+        const val DATAWEDGE_SCAN_EXTRA_SOURCE = "com.symbol.datawedge.source"
         const val DATAWEDGE_SCAN_EXTRA_LABEL_TYPE = "com.symbol.datawedge.label_type"
         const val DATAWEDGE_SEND_CREATE_PROFILE = "com.symbol.datawedge.api.CREATE_PROFILE"
         const val DATAWEDGE_SEND_SET_CONFIG = "com.symbol.datawedge.api.SET_CONFIG"

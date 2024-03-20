a7b090afd6e10c802c0ad270f16b6d0ecc533fc3
Isaac Olajos <isaacolajos@hotmail.com>
2024-03-20 08:34:41 -0400

    Fix datepicker not setting value

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    String example1 = 'https://fml.dev';
+    String example1 = 'https://test.appdaddy.co';
     //String example2 = 'file://fmlpad';
     //String example3 = 'file://example';
 

00e98ce04a54bfcbf034f581f763e8d0f1cf0ee3
x2ecom <jeff@x2ecom.com>
2024-04-02 14:36:29 -0400

    branded app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,10 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    String example1 = 'https://fml.dev';
-    // String example2 = 'file://fmlpad';
-    // String example3 = 'file://example';
+    String example1 = 'http://10.69.4.245:81/';
+    //String example1 = 'https://test.appdaddy.co';
+    //String example1 = 'http://ludapsweb.ec.goodyear.com:81/';
+    //String example1 = 'https://fml.dev';
+    //String example2 = 'file://fmlpad';
+    //String example3 = 'file://example';
 
     var version = "3.0.0";
 
@@ -27,6 +29,7 @@ class MyApp extends StatelessWidget {
             color: Colors.lightBlue,
             brightness: Brightness.light,
             font: 'Roboto',
-            transition: PageTransitions.platform).launch();
+            transition: PageTransitions.platform)
+        .launch();
   }
 }

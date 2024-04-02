e87563c80c9ea465d6e45a30d206ef199082f661
x2ecom <jeff@x2ecom.com>
2024-04-02 12:55:17 -0400

    branded app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,25 +11,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //String example1 = 'http://10.69.4.245:81/';
-    String example1 = 'https://test.appdaddy.co';
-    //String example1 = 'http://ludapsweb.ec.goodyear.com:81/';
-    //String example1 = 'https://fml.dev';
-    //String example2 = 'file://fmlpad';
-    //String example3 = 'file://example';
+
+    String example1 = 'https://fml.dev';
+    // String example2 = 'file://fmlpad';
+    // String example3 = 'file://example';
 
     var version = "3.0.0";
 
     // launch the FML engine
     return FmlEngine(
-            ApplicationType. branded,
+            ApplicationType. multi,
             domain: example1,
             title: "Flutter Markup Language V$version",
             version: version,
             color: Colors.lightBlue,
-            brightness: Brightness.dark,
+            brightness: Brightness.light,
             font: 'Roboto',
-            transition: PageTransitions.platform)
-        .launch();
+            transition: PageTransitions.platform).launch();
   }
 }

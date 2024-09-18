62df8f7b21e1fb74dba6aff3daac899688ec4bc4
x2ecom <jeff@x2ecom.com>
2024-09-18 08:19:25 -0400

    Added <ICONS /> datasource

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\isaac\Documents\Developer\Languages\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\isaac\Documents\Developer\GitHub\flutter_markup_language"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\jeff\Documents\Development\AppDaddy\framework-markup-language"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=3.2.0"
-export "FLUTTER_BUILD_NUMBER=22"
+export "FLUTTER_BUILD_NAME=3.3.1"
+export "FLUTTER_BUILD_NUMBER=24"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
---
+++ lib/datasources/icons/icons_data_model.dart
@@ -0,0 +1,33 @@
+// © COPYRIGHT 2022 APPDADDY SOFTWARE SOLUTIONS INC. ALL RIGHTS RESERVED.
+import 'package:fml/data/data.dart';
+import 'package:fml/datasources/datasource_interface.dart';
+import 'package:fml/graphics.dart';
+import 'package:fml/widgets/widget/model.dart';
+import 'package:fml/datasources/base/model.dart';
+import 'package:xml/xml.dart';
+import 'package:fml/helpers/helpers.dart';
+
+class IconsDataModel extends DataSourceModel implements IDataSource {
+
+  @override
+  bool get autoexecute => true;
+
+  IconsDataModel(super.parent, super.id, {dynamic datastring});
+
+  static IconsDataModel? fromXml(Model parent, XmlElement xml) {
+    IconsDataModel? model = IconsDataModel(parent, Xml.get(node: xml, tag: 'id'));
+    model.deserialize(xml);
+    model.data = generate();
+    return model;
+  }
+
+  static Data generate() {
+    Data data = Data();
+    for (var key in Graphics.icons.keys) {
+      var row = <String, dynamic>{};
+      row["icon"] = key;
+      data.add(row);
+    }
+    return data;
+  }
+}

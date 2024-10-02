e6ed975cd3a703751f5852e4f5ee1c22e7f5652d
Isaac Olajos <isaacolajos@hotmail.com>
2024-10-02 12:41:51 -0400

    3.3.2+25

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     //String example = 'http://lawapsweb.law.goodyear.com:8081/';
     //String example = 'https://pad.fml.dev';
 
-    var version = "3.3.1";
+    var version = "3.3.2";
 
     // launch the FML engine
     return FmlEngine(
